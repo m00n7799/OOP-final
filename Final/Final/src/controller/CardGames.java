4bf0f64f7f2bb5c323617dccd4642939d0d18ea1
m00n7799 <deastwood@student.neumont.edu>
2018-02-26 14:36:25 -0700

    ugh

--- Final/Final/src/controller/CardGames.java
+++ Final/Final/src/controller/CardGames.java
@@ -7,8 +7,10 @@ public class CardGames {
 
 	public void runBlackjack() {
 		BlackJack BJ = new BlackJack();
+		System.out.println("Hello");
 	}
 	public void runHiLo() {
 		HiLo HL = new HiLo();
+		System.out.println("Hi");
 	}
 }
