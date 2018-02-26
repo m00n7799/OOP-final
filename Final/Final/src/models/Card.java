ee521721a4fe3e84247eb0bb74b8aa68bc065aaa
CarlWarrenJr <carlwarrenjr@yahoo.com>
2018-02-26 14:43:34 -0700

    Added deck creater and shuffle

--- Final/Final/src/models/Card.java
+++ Final/Final/src/models/Card.java
@@ -1,14 +1,19 @@
 
 package models;
+import enums.Rank;
 import enums.Suit;
 public class Card {
-private int value;
+private Rank value;
 private Suit suit;
 private boolean hidden;
-public int getValue() {
+public Card(Suit suit, Rank rank) {
+	setSuit(suit);
+	setValue(rank);
+}
+public Rank getValue() {
 	return value;
 }
-public void setValue(int value) {
+public void setValue(Rank value) {
 	this.value=value;
 }
 public Suit getSuit() {
@@ -25,5 +30,13 @@ public void Hidden(boolean hidden) {
 }
 public void flip() {
 	hidden = !hidden;
+}
+@Override
+public String toString() {
+	String cardval="";
+	if(!hidden) {
+		cardval=getValue()+" of "+getSuit();
+	}
+	return cardval;
 }
 }
--- Final/Final/src/models/Deck.java
+++ Final/Final/src/models/Deck.java
@@ -1,5 +1,30 @@
 package models;
 
-public class Deck {
+import java.util.ArrayList;
+import java.util.Random;
 
+import enums.Rank;
+import enums.Suit;
+
+public class Deck {
+ 	private ArrayList<Card> freshDeck;
+	private ArrayList<Card> shuffledDeck;
+	public Deck() {
+		freshDeck = new ArrayList<Card>();
+		shuffledDeck = new ArrayList<Card>();
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				freshDeck.add(new Card(suit,rank));
+			}
+		}
+		shuffle();
+	}
+	public void shuffle() {
+		Random rand=new Random();
+		int gen;
+		for(int i=0;i<52;i++) {
+			gen=rand.nextInt(freshDeck.size());
+			shuffledDeck.add(freshDeck.remove(gen));
+		}
+	}
 }
