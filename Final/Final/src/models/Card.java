e402cb22b9ea348e33f677287f00b73f1ec3c2cb
CarlWarrenJr <carlwarrenjr@yahoo.com>
2018-02-26 13:37:18 -0700

    Added card logic

--- Final/Final/src/models/Card.java
+++ Final/Final/src/models/Card.java
@@ -1,6 +1,29 @@
 
 package models;
-
+import enums.Suit;
 public class Card {
-
+private int value;
+private Suit suit;
+private boolean hidden;
+public int getValue() {
+	return value;
+}
+public void setValue(int value) {
+	this.value=value;
+}
+public Suit getSuit() {
+	return suit;
+}
+public void setSuit(Suit suit) {
+	this.suit=suit;
+}
+public boolean isHidden() {
+	return hidden;
+}
+public void Hidden(boolean hidden) {
+	this.hidden=hidden;
+}
+public void flip() {
+	hidden = !hidden;
+}
 }
