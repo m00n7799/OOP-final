96104f65e8f58fa07b46ae11f11317da9247c2bb
luis-padilla <33160159+luis-padilla@users.noreply.github.com>
2018-02-26 13:15:09 -0700

    suit

--- Final/Final/src/application/Main.java
+++ Final/Final/src/application/Main.java
@@ -1,45 +1,47 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
-			StackPane root = new StackPane();
-			Scene scene = new Scene(root,1000,400);
-			Button blackJack = new Button("Play BlackJack");
-			Button hiLo = new Button("Play HiLo");
-			
-			blackJack.setOnAction(new EventHandler<ActionEvent>() {
-				
-				@Override
-				public void handle(ActionEvent event) {
-					System.out.println("Playing BlackJack");
-					
-				}
-			});
-			
-			hiLo.setOnAction(new EventHandler<ActionEvent>(){
-				
-				@Override
-				public void handle(ActionEvent event) {
-					System.out.println("Playing Hilo");
-				}
-			});
-			root.getChildren().addAll(blackJack,hiLo);
-			primaryStage.setScene(scene);
-			primaryStage.show();		
+		StackPane root = new StackPane();
+		Scene scene = new Scene(root, 600, 600);
+
+		// BlackJack Button
+		Button blackJack = new Button("Play BlackJack");
+		
+		blackJack.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Playing BlackJack");
+
+			}
+		});
+		
+		// HiLo Button
+		Button hiLo = new Button("Play HiLo");
+		hiLo.relocate(200, 500);
+		hiLo.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Playing Hilo");
+			}
+		});
+		root.getChildren().addAll(blackJack, hiLo);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
