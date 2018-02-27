9eea99ddbc81b22b64d7b9af1547c7ea9c1939a1
CarlWarrenJr <carlwarrenjr@yahoo.com>
2018-02-27 14:06:03 -0700

    Gave the cards numerical value

--- Final/Final/src/models/Card.java
+++ Final/Final/src/models/Card.java
@@ -6,6 +6,7 @@ public class Card {
 private Rank value;
 private Suit suit;
 private boolean hidden;
+private int cardValue;
 public Card(Suit suit, Rank rank) {
 	setSuit(suit);
 	setValue(rank);
@@ -30,6 +31,56 @@ public void Hidden(boolean hidden) {
 }
 public void flip() {
 	hidden = !hidden;
+}
+public int getCardValue() {
+	return cardValue;
+}
+public void setCardValue(Rank value) {
+	switch(value) {
+	case ACE:
+		this.cardValue = 1;
+		break;
+	case TWO:
+		this.cardValue = 2;
+		break;
+	case THREE:
+		this.cardValue = 3;
+		break;
+	case FOUR:
+		this.cardValue = 4;
+		break;
+	case FIVE:
+		this.cardValue = 5;
+		break;
+	case SIX:
+		this.cardValue = 6;
+		break;
+	case SEVEN:
+		this.cardValue = 7;
+		break;
+	case EIGHT:
+		this.cardValue = 8;
+		break;
+	case NINE:
+		this.cardValue = 9;
+		break;
+	case TEN:
+		this.cardValue = 10;
+		break;
+	case JACK:
+		this.cardValue = 10;
+		break;
+	case QUEEN:
+		this.cardValue = 10;
+		break;
+	case KING:
+		this.cardValue = 10;
+		break;	
+	default:
+		break;
+	
+	}
+	
 }
 @Override
 public String toString() {
