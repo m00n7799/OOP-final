7b66a3bf2e10b5a119bfb8966b5d2fe91bee371f
m00n7799 <deastwood@student.neumont.edu>
2018-03-08 23:15:36 -0700

    Revert "Nono"
    This reverts commit 730719b41b7dcae053b55d11e54097df61bf836c.

--- Final/Final/src/models/Deck.java
+++ Final/Final/src/models/Deck.java
@@ -70,9 +70,14 @@ public class Deck {
 		ArrayList<Card> hand = new ArrayList<Card>();
 		// shuffledDeck.get(0).Hidden(false);
 <<<<<<< HEAD
+<<<<<<< HEAD
 		Card card = null;
 		try {
 			card=shuffledDeck.get(0);
+=======
+		Card card = shuffledDeck.get(0);
+		try {
+>>>>>>> parent of 730719b... Nono
 =======
 		Card card = shuffledDeck.get(0);
 		try {
@@ -86,11 +91,14 @@ public class Deck {
 			hand.add(card);
 			p.setHand(hand);
 <<<<<<< HEAD
+<<<<<<< HEAD
 		}catch (IndexOutOfBoundsException e) {
 			newDeck();
 			card = shuffledDeck.get(0);
 			hand.add(card);
 			p.setHand(hand);
+=======
+>>>>>>> parent of 730719b... Nono
 =======
 >>>>>>> parent of 730719b... Nono
 		}
